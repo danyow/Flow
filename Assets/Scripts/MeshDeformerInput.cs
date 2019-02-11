253d4f8459d4ed44f15662e760a314f1bce68942
Danyow <i.zuucol@gmail.com>
2019-02-11 22:46:17 +0800

    完成形变

---
+++ Assets/Scripts/MeshDeformerInput.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class MeshDeformerInput : MonoBehaviour
+{
+    public float force = 10f;
+    public float forceOffset = 0.1f;
+
+    private void Update() {
+        if (Input.GetMouseButton(0)) {
+            HandheldInput();
+        }
+    }
+
+    void HandheldInput() {
+        Ray inputRay = Camera.main.ScreenPointToRay(Input.mousePosition);
+        RaycastHit hit;
+        if (Physics.Raycast(inputRay, out hit)) {
+            MeshDeformer deformer = hit.collider.GetComponent<MeshDeformer>();
+            if (deformer) {
+                Vector3 point = hit.point;
+                point += hit.normal * forceOffset;
+                deformer.AddDeformingForce(point, force);
+            }
+        }
+    }
+}
